ee39afd6689c0415b5c7a7a57232adf6145d84fb
Joao Paulo Alvarenga <joao1310al@gmail.com>
2024-04-12 02:04:01 -0300

    todo item partial iterates over item array

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -4,20 +4,34 @@ class TodoListsController < ApplicationController
       {
         id: 1,
         title: 'this is the first list',
-        item: {
-          id: 1,
-          text: 'stuff',
-          checked: true
-        }
+        items: [
+          {
+            id: 1,
+            text: 'stuff',
+            checked: true
+          },
+          {
+            id: 2,
+            text: 'stuff 1',
+            checked: false
+          },
+        ]
       },
       {
         id: 2,
         title: 'this is the second list',
-        item: {
-          id: 1,
-          text: 'stuff 1',
-          checked: false
-        }
+        items: [
+          {
+            id: 1,
+            text: 'stuff 1',
+            checked: false
+          },
+          {
+            id: 2,
+            text: 'stuff 2',
+            checked: true
+          },
+        ]
       }
     ]
     @todo_lists = todo_lists_mock_object
